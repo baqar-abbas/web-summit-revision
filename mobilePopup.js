e4e08a8a7307f6688b1bc7ac82425eea3213464c
baqar-abbas <baqar007@hotmail.com>
2023-11-19 08:21:51 +0500

    fix linter errors

--- mobilePopup.js
+++ mobilePopup.js
@@ -1,22 +1,20 @@
-/*eslint-disable*/
+const navMenu = document.querySelector('.nav-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const cross = document.querySelector('#cross');
+const hamburger = document.querySelector('.imageone');
 
-const navMenu = document.querySelector(".nav-menu");
-const mobileMenu = document.querySelector(".mobile-menu");
-const cross = document.querySelector("#cross");
-const hamburger = document.querySelector(".imageone");
-
-hamburger.addEventListener("click", (event) => {
-    // mobileMenu.style.display = "flex";
-    mobileMenu.style.width = "100vw";
-    navMenu.style.display = "none";
-    event.preventDefault();
-})
+hamburger.addEventListener('click', (event) => {
+  // mobileMenu.style.display = "flex";
+  mobileMenu.style.width = '100vw';
+  navMenu.style.display = 'none';
+  event.preventDefault();
+});
 
 const menuClose = () => {
-    navMenu.style.display = "block";
-    // mobileMenu.style.display = "none";
-    mobileMenu.style.width = "0vw";
-}
+  navMenu.style.display = 'block';
+  // mobileMenu.style.display = "none";
+  mobileMenu.style.width = '0vw';
+};
 
-cross.addEventListener("click", menuClose);
-mobileMenu.addEventListener("click", menuClose);
+cross.addEventListener('click', menuClose);
+mobileMenu.addEventListener('click', menuClose);
