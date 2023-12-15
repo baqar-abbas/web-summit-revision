c6f66e47ed2b3a68e68e6fe748be186d24912f3e
baqar-abbas <baqar007@hotmail.com>
2023-12-15 21:48:13 +0500

    fix linter errors

--- aboutPopup.js
+++ aboutPopup.js
@@ -1,21 +1,20 @@
-/* eslint-disable */
 const navMenu = document.querySelector('.nav-menu-about');
 const hamMenu = document.querySelector('.imageone-about');
 const cross = document.getElementById('cross');
 const mobileMenu = document.querySelector('.mobile-menu');
 
-hamMenu.addEventListener('click', show);
-cross.addEventListener('click', hide);
-
 function show() {
-    // mobileMenu.style.display = 'flex';
-    mobileMenu.style.width = '100vw';
-    navMenu.style.display = 'none';
-    cross.style.cursor = 'pointer';
+  // mobileMenu.style.display = 'flex';
+  mobileMenu.style.width = '100vw';
+  navMenu.style.display = 'none';
+  cross.style.cursor = 'pointer';
 }
 
 function hide() {
-    // mobileMenu.style.display = 'none';
-    mobileMenu.style.width = '0vw';
-    navMenu.style.display = 'block';
+  // mobileMenu.style.display = 'none';
+  mobileMenu.style.width = '0vw';
+  navMenu.style.display = 'block';
 }
+
+hamMenu.addEventListener('click', show);
+cross.addEventListener('click', hide);
